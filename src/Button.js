5bcfd05a82b8e0cc50081ab1964a20b9354d1a03
Liroo <pierre-monge@hotmail.fr>
2016-08-02 17:35:38 +0200

    Init ex

---
+++ src/Button.js
@@ -0,0 +1,46 @@
+// @flow
+import React, { Component, PropTypes } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: 'rgb(161, 235, 208)',
+    // margin: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  label: {
+    fontSize: 16,
+  },
+});
+
+type ButtonProps = {
+  label: string,
+  onPress: Function,
+  style?: Object,
+};
+
+export default class Button extends Component {
+  props: ButtonProps;
+
+  static propTypes = {
+    label: PropTypes.string,
+    onPress: PropTypes.func.isRequired,
+    style: View.propTypes.style,
+  };
+
+  render() {
+    const { label, style, onPress } = this.props;
+    return (
+      <TouchableOpacity style={[styles.container, style]} onPress={ onPress }>
+        <Text style={styles.label}>{label}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
